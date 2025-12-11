dfc1759c094654bdbaa7ab9ccf358bfcd5941c32
pshrote1-afk <pshrote1@gmail.com>
2025-12-11 03:31:45 +0000

    #2 POST form data, and handle with @Controller

--- ch2/src/main/java/com/learn/spring/boot/controllers/HomeController.java
+++ ch2/src/main/java/com/learn/spring/boot/controllers/HomeController.java
@@ -3,8 +3,10 @@ package com.learn.spring.boot.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.learn.spring.boot.services.*;
-
+import com.learn.spring.boot.pojos.*;
 @Controller
 public class HomeController {
 
@@ -21,4 +23,12 @@ public class HomeController {
         model.addAttribute("videos", videoService.getVideos());
         return "home";
     }
+
+    @PostMapping("/new-video")
+    public String newVideo(@RequestParam String name)
+    {
+	videoService.addVideo(new Video(name));
+	return "redirect:/";
+    }
+
 }
--- ch2/src/main/java/com/learn/spring/boot/services/VideoService.java
+++ ch2/src/main/java/com/learn/spring/boot/services/VideoService.java
@@ -1,15 +1,21 @@
 package com.learn.spring.boot.services;
 
-import java.util.List;
+import java.util.*;
 import com.learn.spring.boot.pojos.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VideoService
 {
-private List<Video> videos = List.of(new Video("Vid 1"), new Video("Some Vid 2"), new Video("Some video which has a lenghty, long name 3"));
+private List<Video> videos = new ArrayList<>(List.of(new Video("Vid 1"), new Video("Some Vid 2"), new Video("Some video which has a lenghty, long name 3")));
 public List<Video> getVideos()
 {
 return this.videos;
 }
+
+public void addVideo(Video video)
+{
+videos.add(video);
+}
+
 }
