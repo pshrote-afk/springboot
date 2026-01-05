7050a84faf03fa01f6f7f779992811c1d6a0adea
pshrote1-afk <pshrote1@gmail.com>
2026-01-05 01:58:41 +0000

    6. add entity to query from db, and repository to gather operations in one place

--- ch2/src/main/java/com/learn/spring/boot/Chapter2Application.java
+++ ch2/src/main/java/com/learn/spring/boot/Chapter2Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter2Application
-{
-public static void main(String gg[])
-{
-SpringApplication.run(Chapter2Application.class,gg);
-}
+public class Chapter2Application {
+    public static void main(String gg[]) 
+    {
+        SpringApplication.run(Chapter2Application.class, gg);
+    }
 }
---
+++ ch2/src/main/java/com/learn/spring/boot/services/VideoRepository.java
@@ -0,0 +1,9 @@
+package com.learn.spring.boot.services;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.learn.spring.boot.pojos.VideoEntity;
+
+public interface VideoRepository //for writing a query, w/o writing a query
+    extends JpaRepository<VideoEntity, Long> 
+{
+
+}
